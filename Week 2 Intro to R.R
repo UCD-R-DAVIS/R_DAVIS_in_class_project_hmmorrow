02e1a39fdc2db7ad441c45a8e94287cc2531419c
Harrison Morrow <hmmorrow@ucdavis.edu>
2024-10-03 13:56:27 -0700

    in class

---
+++ Week 2 Intro to R.R
@@ -0,0 +1,73 @@
+3+4
+4+8*2
+4+8*3^2
+
+#this is a comment
+4e3
+exp(1)
+?exp
+log(100)
+log(100, base=10)
+log(x)
+y<-2
+log(100, 10
+    )
+x+y
+
+x
+y
+log(x)
+x
+rm(x)
+rm(y)
+log(-40)
+x=2
+x==5
+x!=5
+x<=4
+x=x+1
+y=x+2
+x+y
+numSamples=50
+num_samples=50
+numsamples=40
+rm(numsamples)
+rm(numSamples)
+a <- 1
+b <- 2
+c <- a + b
+b <- 4
+a <- b
+c <- a
+c 
+kg=2.2
+2kg
+2*kg
+elephant1_kg <- 3492
+elephant2_lb <- 7757
+el
+elephant1_kg*2.2
+2*
+5
+2*5
+log(x=2, base=4)
+x=1
+rm(x)
+sqrt(e4)
+sqrt(exp(4))
+mynumber=6
+mynumber==5
+mynumber=>5
+mynumber>=5
+numSamples=50
+num_samples=40
+rm(num_samples)
+rm(list=ls())
+
+elephant1_kg=3492
+elephant2_lb=7757
+elephant1_kg*2.2
+(elephant1_lb=elephant1_kg*2.2)
+elephant1_lb>elephant2_lb
+myelephants =c(elephant1_lb,elephant2_lb)
+which(myelephants==max(myelephants))
