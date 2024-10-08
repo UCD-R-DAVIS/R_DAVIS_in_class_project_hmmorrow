005f97af5f56feafb6569b97d411430194b4e961
Harrison Morrow <hmmorrow@ucdavis.edu>
2024-10-08 15:59:08 -0700

    week 2 homework completed

---
+++ Scripts/Week 2 Intro to R.R
@@ -0,0 +1,101 @@
+3+4
+4+8*2
+4+8*3^2
+
+#this is a comment
+4e3
+exp(1)
+?exp
+log(100)
+log(100, base=10)
+log(x)
+y<-2
+log(100, 10
+    )
+x+y
+
+x
+y
+log(x)
+x
+rm(x)
+rm(y)
+log(-40)
+x=2
+x==5
+x!=5
+x<=4
+x=x+1
+y=x+2
+x+y
+numSamples=50
+num_samples=50
+numsamples=40
+rm(numsamples)
+rm(numSamples)
+a <- 1
+b <- 2
+c <- a + b
+b <- 4
+a <- b
+c <- a
+c 
+kg=2.2
+2kg
+2*kg
+elephant1_kg <- 3492
+elephant2_lb <- 7757
+el
+elephant1_kg*2.2
+2*
+5
+2*5
+log(x=2, base=4)
+x=1
+rm(x)
+sqrt(e4)
+sqrt(exp(4))
+mynumber=6
+mynumber==5
+mynumber=>5
+mynumber>=5
+numSamples=50
+num_samples=40
+rm(num_samples)
+rm(list=ls())
+
+elephant1_kg=3492
+elephant2_lb=7757
+elephant1_kg*2.2
+(elephant1_lb=elephant1_kg*2.2)
+elephant1_lb>elephant2_lb
+myelephants =c(elephant1_lb,elephant2_lb)
+which(myelephants==max(myelephants))
+getwd()
+d=read.csv("./data/tail_length.csv")
+dir.create("./lectures")
+weight_g=c(50,60,65,82)
+rm(weight)
+animals=c("mouse","rat","dog")
+length(weight_g)
+str(weight_g)
+weight_g=c(weight_g,90)
+weight_g
+num_char=c(1,2,3,"a")
+num_logical=c(1,2,3,TRUE)
+char_logical=c("a","b","c", TRUE)
+tricky=c(1,2,3,"4")
+num_logical=c(1,2,3,TRUE)
+combined_logical=c(num_logical,char_logical)
+num_char
+num_logical
+char_logical
+##Subsetting ---
+
+animals[2]
+animals[c(2,3]
+animals[c(2,3)]
+weight_g=c(21,34,39,54,55)
+weight_g>50
+weight_g[weight_g>50]
+animals%in%c("rat","cat", "dog","duck","goat")
